e4cb10550e52f7b329c9f26a8a9d035734d1e562
vvmk <vincentmasiello@gmail.com>
2018-04-16 20:54:50 -0400

    Stub and write tests

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,15 @@
 package io.zipcoder;
 
 public class Problem5 {
+    public static String reverseAndCapitalize(String input) {
+        return null;
+    }
+
+    public static String reverseString(String input) {
+        return null;
+    }
+
+    public static String capitalize(String input) {
+        return null;
+    }
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,37 @@
 package io.zipcoder;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Problem5Test {
+    @Test
+    void testReverseOneWord() {
+        String input = "Reverse";
+
+        String expected = "esreveR";
+        String actual = Problem5.reverseString(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCapitalizeOneWord() {
+        String input = "capital";
+
+        String expected = "Capital";
+        String actual = Problem5.capitalize(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testReverseAndCapitalizeWord() {
+        String input = "reverseandcap";
+
+        String expected = "Pacdnaesrever";
+        String actual = Problem5.reverseAndCapitalize(input);
+        
+        assertEquals(expected, actual);
+    }
 }

45f1a8304d64bc27bded9851f90f3e3d66fa36e3
vvmk <vincentmasiello@gmail.com>
2018-04-16 21:13:21 -0400

    Recursin' and reversin'

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -6,7 +6,10 @@ public class Problem5 {
     }
 
     public static String reverseString(String input) {
-        return null;
+        if (input.length() == 1)
+            return input;
+        else
+            return input.substring(input.length()-1) + reverseString(input.substring(0, input.length()-1));
     }
 
     public static String capitalize(String input) {
